0342d4865266326b67948972a57a0d38ac29feb0
TatsuKenshi <rockinbg@gmail.com>
2022-12-01 23:14:41 +0100

    class basics, private & public fields, getters, setters

--- 03-pure-ts/dist/index.js
+++ 03-pure-ts/dist/index.js
@@ -1,3 +1,65 @@
 "use strict";
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+    // also, all properties not marked as private are public
+    constructor(email, name, street) {
+        // since city is not in constructor, it needs initial value
+        this.city = "";
+        this.email = email;
+        this.name = name;
+        this.street = street;
+    }
+}
+const miki = new User("a@a.com", "miki", "Husova 12");
+miki.city = "London";
+console.log(miki);
+// we can't log miki.street because street is private
+// console.log(miki.street);
+//
+// // //
+// Classes shorter syntax
+// // //
+//
+class User2 {
+    constructor(name, street) {
+        this.name = name;
+        this.street = street;
+        this.email = "a@a.com";
+    }
+}
+const miki2 = new User2("miki", "Husova 12");
+console.log(miki2);
+//
+// // //
+// Getters and Setters
+// // //
+//
+// getters and setters are mainly there to give limited access to private properties outside the class.
+class User3 {
+    constructor(name, email, street) {
+        this.name = name;
+        this.email = email;
+        this.street = street;
+        this._courseCount = 1;
+        this.city = "London";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        // setters can't have anything as return type
+        if (courseNumber <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNumber;
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+}
+const miki3 = new User3("miki", "a@a.com", "Husova 12");
+console.log(miki3);
+// TS doesn't allow acccess to the deleteToken private method outside it's native class
+// console.log(miki3.deleteToken);
--- 03-pure-ts/src/index.ts
+++ 03-pure-ts/src/index.ts
@@ -1,2 +1,84 @@
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+  email: string;
+  name: string;
+
+  // since city is not in constructor, it needs initial value
+  city: string = "";
+
+  // private properties are not accessible outside the class
+  // you can either use the private keyword or a hashtag (#)
+  private street: string;
+  // also, all properties not marked as private are public
+
+  constructor(email: string, name: string, street: string) {
+    this.email = email;
+    this.name = name;
+    this.street = street;
+  }
+}
+
+const miki = new User("a@a.com", "miki", "Husova 12");
+miki.city = "London";
+
+console.log(miki);
+
+// we can't log miki.street because street is private
+// console.log(miki.street);
+
+//
+// // //
+// Classes shorter syntax
+// // //
+//
+class User2 {
+  readonly email: string = "a@a.com";
+  constructor(public name: string, private street: string) {}
+}
+
+const miki2 = new User2("miki", "Husova 12");
+console.log(miki2);
+
+//
+// // //
+// Getters and Setters
+// // //
+//
+
+// getters and setters are mainly there to give limited access to private properties outside the class.
+
+class User3 {
+  private _courseCount = 1;
+
+  readonly city: string = "London";
+  constructor(
+    public name: string,
+    public email: string,
+    private street: string
+  ) {}
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNumber) {
+    // setters can't have anything as return type
+    if (courseNumber <= 1) {
+      throw new Error("course count should be more than 1");
+    }
+    this._courseCount = courseNumber;
+  }
+
+  private deleteToken() {
+    console.log("token deleted");
+  }
+}
+
+const miki3 = new User3("miki", "a@a.com", "Husova 12");
+console.log(miki3);
+
+// TS doesn't allow acccess to the deleteToken private method outside it's native class
+// console.log(miki3.deleteToken);
